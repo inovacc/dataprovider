cdbae84c04a8e5bcf025102e6933fc3f32e3ee96
Dyam Marcano <dyam.marcano@gmail.com>
2024-07-22 15:47:32 -0300

    implement queries

--- internal/provider/queries.go
+++ internal/provider/queries.go
@@ -13,6 +13,7 @@ type SQLBuilder struct {
 	driver    string
 	columns   []string
 	values    []string
+	set       []string
 	where     []string
 	joins     []string
 	groupBy   []string
@@ -44,10 +45,73 @@ func (b *SQLBuilder) Select(columns ...string) *SQLBuilder {
 	return b
 }
 
+type CreateTableColumn struct {
+	name     string
+	dataType string
+	options  []string
+}
+
+// CreateTable establece la tabla para la consulta CREATE TABLE
+func (b *SQLBuilder) CreateTable(table string) *SQLBuilder {
+	b.queryType = "CREATE TABLE"
+	b.table = table
+	return b
+}
+
+// IfNotExists agrega la opción IF NOT EXISTS a la consulta CREATE TABLE
+func (b *SQLBuilder) IfNotExists() *SQLBuilder {
+	b.columns = append(b.columns, "IF NOT EXISTS")
+	return b
+}
+
+// Column agrega una columna a la consulta CREATE TABLE
+func (b *SQLBuilder) Column(name string) *CreateTableColumn {
+	return &CreateTableColumn{name: name}
+}
+
+// Type establece el tipo de dato de la columna
+func (c *CreateTableColumn) Type(dataType string) *CreateTableColumn {
+	c.dataType = dataType
+	return c
+}
+
+// PrimaryKey establece la columna como PRIMARY KEY
+func (c *CreateTableColumn) PrimaryKey() *CreateTableColumn {
+	c.options = append(c.options, "PRIMARY KEY")
+	return c
+}
+
+// Columns agrega las columnas a la consulta CREATE TABLE
+func (b *SQLBuilder) Columns(columns ...*CreateTableColumn) *SQLBuilder {
+	for _, column := range columns {
+		colDef := fmt.Sprintf("%s %s %s", column.name, column.dataType, strings.Join(column.options, " "))
+		b.columns = append(b.columns, strings.TrimSpace(colDef))
+	}
+	return b
+}
+
 // Insert establece la tabla y columnas para la consulta INSERT
 func (b *SQLBuilder) Insert(columns ...string) *SQLBuilder {
 	b.queryType = "INSERT"
 	b.columns = columns
+	return b
+}
+
+// Update establece la tabla para la consulta UPDATE
+func (b *SQLBuilder) Update() *SQLBuilder {
+	b.queryType = "UPDATE"
+	return b
+}
+
+// Set establece las columnas y valores para la consulta UPDATE
+func (b *SQLBuilder) Set(values map[string]any) *SQLBuilder {
+	for column, value := range values {
+		val := fmt.Sprintf("%v", value)
+		if str, ok := value.(string); ok {
+			val = fmt.Sprintf("'%s'", str)
+		}
+		b.set = append(b.set, fmt.Sprintf("%s = %s", column, val))
+	}
 	return b
 }
 
@@ -122,6 +186,29 @@ func (b *SQLBuilder) Build() string {
 		sb.WriteString(strings.Join(b.columns, ", "))
 		sb.WriteString(") VALUES (")
 		sb.WriteString(strings.Join(b.values, ", "))
+		sb.WriteString(")")
+	case "UPDATE":
+		sb.WriteString("UPDATE ")
+		if b.schema != "" {
+			sb.WriteString(b.schema)
+			sb.WriteString(".")
+		}
+		sb.WriteString(b.table)
+		sb.WriteString(" SET ")
+		sb.WriteString(strings.Join(b.set, ", "))
+	case "CREATE TABLE":
+		sb.WriteString("CREATE TABLE ")
+		if len(b.columns) > 0 && b.columns[0] == "IF NOT EXISTS" {
+			sb.WriteString("IF NOT EXISTS ")
+			b.columns = b.columns[1:]
+		}
+		if b.schema != "" {
+			sb.WriteString(b.schema)
+			sb.WriteString(".")
+		}
+		sb.WriteString(b.table)
+		sb.WriteString(" (")
+		sb.WriteString(strings.Join(b.columns, ", "))
 		sb.WriteString(")")
 	default:
 		return ""
--- queries_test.go
+++ queries_test.go
@@ -44,3 +44,48 @@ func TestQuerySchema(t *testing.T) {
 		t.Error("SELECT Query is not correct")
 	}
 }
+
+func TestQueryCreate(t *testing.T) {
+	provider := Must(NewDataProvider(NewOptions(WithMemoryDB())))
+
+	query := provider.SqlBuilder().
+		CreateTable("users").
+		IfNotExists().
+		Columns(
+			provider.SqlBuilder().Column("id").Type("SERIAL").PrimaryKey(),
+			provider.SqlBuilder().Column("first_name").Type("TEXT"),
+			provider.SqlBuilder().Column("last_name").Type("TEXT"),
+			provider.SqlBuilder().Column("email").Type("TEXT"),
+			provider.SqlBuilder().Column("ip_address").Type("TEXT"),
+			provider.SqlBuilder().Column("city").Type("TEXT"),
+		).
+		Build()
+
+	t.Log("CREATE TABLE Query:")
+	t.Log(query)
+
+	if query != "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, first_name TEXT, last_name TEXT, email TEXT, ip_address TEXT, city TEXT)" {
+		t.Error("CREATE TABLE Query is not correct")
+	}
+}
+
+func TestQueryUpdate(t *testing.T) {
+	provider := Must(NewDataProvider(NewOptions(WithMemoryDB())))
+
+	query := provider.SqlBuilder().
+		Table("users").
+		Update().
+		Set(map[string]any{
+			"name":  "John Doe",
+			"email": "test@admin.com",
+		}).
+		Where("id = 1").
+		Build()
+
+	t.Log("UPDATE Query:")
+	t.Log(query)
+
+	if query != "UPDATE users SET name = 'John Doe', email = 'test@admin.com' WHERE id = 1" {
+		t.Error("UPDATE Query is not correct")
+	}
+}
